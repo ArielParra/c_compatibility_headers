baee43c1f7da14141e94dce4eb5004165d6d5d9c
Ariel Parra Windows <al280862@edu.uaa.mx>
2023-11-02 23:31:48 -0600

    Play_Sound fixes

--- Play_Sound.h
+++ Play_Sound.h
@@ -12,23 +12,15 @@ extern "C" {
 /*OS detection*/
 #if defined(_WIN32) || defined(_CYGWIN_)
 
-
-    /* Warnings!
-    Carefull with the music file routes on Windows, you need four backslashes per backslash in the system() function.
-    Might work wiht normal slashes in newer Windows using pwsh, but may need them for retrocompatibility in cmd
-    If you modify the SoundCommand,please modify the string size 
-    */
-
     #include <windows.h>//PlaySound()
     //#pragma comment(lib, "winmm") //Microsoft Visual C++ (MSVC) specific macro
     #warning  "Playsound() needs -lwinmm as compiler argument"
 
     //void Play_Sound(const char* file){PlaySound(TEXT(file),NULL,SND_ASYNC);}//PlaySound() will stop while using getch()
-
     DWORD WINAPI ThreadFunction(LPVOID lpParameter) {
         const char* file = (const char*)lpParameter;
-        PlaySoundA(file, NULL, SND_ASYNC); 
-        return 0;
+        PlaySoundA(file, NULL, SND_ASYNC);
+        return 0; 
     }
     #include<stdio.h>//fprintf(),stderr
     void Play_Sound(const char* file) {
@@ -37,19 +29,40 @@ extern "C" {
     }//Play_Sound() will not stop while using getch()
     void Stop_Sound(){PlaySound(NULL, 0, 0);}
 
-    /* Using sox or ffmpeg ffplay command, 
+    /* Using sox or ffmpeg ffplay apps, 
     sox.exe needs 14 dlls to work and only uses 4.96MB in total, 
-    ffplay.exe is standalon without dlls, but it is 77.2MB in size,
+    ffplay.exe is a standalon exe without dlls, but it is 77.2MB in size,
     You can get sox portable from: https://sourceforge.net/projects/sox/files/sox/14.4.2/sox-14.4.2-win32.zip/download
     You can get ffmpeg portable from: https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-essentials.7z
-    */
-
-    //void Play_Sound(const char* file){char SoundCommand[strlen(file)+27+28];strcpy(SoundCommand, "start /b .\\\\sox.exe .\\\\");strcat(SoundCommand, file);strcat(SoundCommand," -t waveaudio -d >NUL 2>&1 &");system(SoundCommand);}
-    //void Stop_Sound(){system("taskkill /F /IM sox.exe");}
     
-    //void Play_Sound(const char* file){char SoundCommand[strlen(file)+38+11];strcpy(SoundCommand, "start /b .\\\\ffplay.exe -nodisp .\\\\");strcat(SoundCommand, file);strcat(SoundCommand,">NUL 2>&1 &");system(SoundCommand);}
-    //void Stop_Sound(){system("taskkill /F /IM ffplay.exe");}
+    Carefull with the music file routes on Windows, you need two backslashes per backslash 
+    in the system() function. Might work wiht normal slashes in newer Windows using pwsh, 
+    but may use convertToWindowsRoute() function for compatibility in cmd
 
+    If you modify the SoundCommand,please modify the string size 
+    */
+    /*
+    void convertToWindowsRoute(char* str) {
+        for (size_t i = 0; i < strlen(str); i++)
+            if (str[i] == '/') str[i] = '\\';
+    }
+    */ 
+    /*
+    void Play_Sound(const char* file){
+        convertToWindowsRoute(file);
+        const char SoundCommand[strlen(file)+23+28];strcpy(SoundCommand, "start /b .\\sox.exe .\\");
+        strcat(SoundCommand, file);strcat(SoundCommand," -t waveaudio -d >NUL 2>&1 &");system(SoundCommand);
+    }
+    void Stop_Sound(){system("taskkill /F /IM sox.exe >NUL 2>&1 &");}
+    */
+    /*
+    void Play_Sound(const char* file){
+        convertToWindowsRoute(file);
+        const char SoundCommand[strlen(file)+34+11];strcpy(SoundCommand, "start /b .\\ffplay.exe -nodisp .\\");
+        strcat(SoundCommand, file);strcat(SoundCommand,">NUL 2>&1 &");system(SoundCommand);
+    }
+    void Stop_Sound(){system("taskkill /F /IM ffplay.exe >NUL 2>&1 &");}
+    */
 #elif defined(__linux__)
 
     //*WSL Linux needs ffplay command from ffmpeg
@@ -79,10 +92,10 @@ extern "C" {
 #endif//OS detection 
 
 #undef PlaySoundA
-#define PlaySoundA Play_Sound
+#define PlaySoundA(file, opt, async) do{ Play_Sound(file); }while(0)
 
 #undef PlaySound
-#define PlaySound Play_Sound
+#define PlaySound(file, opt, async) do{ Play_Sound(file); }while(0)
 
 #ifdef __cplusplus
 }
--- README.md
+++ README.md
@@ -7,8 +7,10 @@
 - [ ] allCompat.h
 - [ ] beep.h
 - [ ] README.md
+- [ ] dependencies
 - [ ] Code comments and pretty identation
 - [ ] man pages for each header file
+    - [ ] roadmap and things Ill not do 
     - [ ] stdlibCompat.h
     - [ ] ansiCompat.h
     - [ ] Play_Sound.h
@@ -19,27 +21,28 @@
 
 ---
 
-
 # FAQ
-ncursesCompat makes some ncurses.h functions work on windows with the use of conio.h and windows.h and turns some ncurses.h functions like getch() function work as Windows conio.h getch() to get a "Unified and expected behavior" between Windows and *NIX
-This may not be sutable for production :P
-stdlibCompat uses deprecated functions in Windows
-this project uses alot of macros to define and redifine functions
-song.wav clip downloaded from https://freemusicarchive.org/music/John_Harrison_with_the_Wichita_State_University_Chamber_Players/The_Four_Seasons_Vivaldi
+- ncursesCompat makes some ncurses.h functions work on windows with the use of conio.h and windows.h and turns some ncurses.h 
+- functions like getch() function work as Windows conio.h getch() to get a "Unified and expected behavior" between Windows and *NIX
+- This may not be sutable for production :P, you may prefer using pdcurses, SDL, or wide characters from wchar.h
+- stdlibCompat uses deprecated functions in Windows
+- this project uses alot of macros to define and redifine functions
+- song.wav clip downloaded from https://freemusicarchive.org/music/John_Harrison_with_the_Wichita_State_University_Chamber_Players/The_Four_Seasons_Vivaldi
 
 # Licence
-No guaranties
-Software is as it is
-it is Open Source, dont know which licence yet
+- No guaranties
+- Software is as it is
+- it is Open Source, dont know which licence yet
+- Beep.h from https://github.com/zserge/beep 
 
 # Warnings
-This project is mainly to use with GCC compiler and Glibc, compatibility with Compilers like LLVM/CLANG and/or other C librarys like ulibc, musl or bionic is unkown and most likely may not work as intended. 
+-This project is mainly to use with GCC compiler and Glibc, compatibility with Compilers like LLVM/CLANG and/or other C librarys like ulibc, musl or bionic is unkown and most likely may not work as intended. 
 
-this project has only been tested in Windows 10, linux and MacOS, BSD or other *NIX will probably works with manual patches
-The GNU make
+- this project has only been tested in Windows 10, linux and MacOS, BSD or other *NIX will probably works with manual patches
+- The GNU make is used with the makefiles so no compatibility with BSD Make
 
-ncursesCompat may need ncurses and/or ncurses-dev package 
-
-Beep may need libasound2-dev package
+# Dependencies
+- ncursesCompat may need ncurses and/or ncurses-dev package
+- Beep may need libasound2-dev package
 
 
